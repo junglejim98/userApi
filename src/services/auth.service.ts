f2ab605b7ed2facb91b4df07cdfe4aac5fba087d
Lysyonok Gregory <bloodamerika@gmail.com>
2025-08-14 14:01:49 +0300

    auth/reg service started to write

---
+++ src/services/auth.service.ts
@@ -0,0 +1,28 @@
+import bcrupt from 'bcryptjs'
+import prisma from '../db/prisma'
+import type { User, Role, Status } from '@prisma/client';
+
+type PublicUser = {
+    id: number;
+    firstName: string;
+    lastName: string;
+    middleName?: string;
+    bierthDate: string;
+    email: string;
+    role: string;
+    status: string;
+};
+
+
+function toPublicUser(u: User & { role: Role; status: Status }): PublicUser {
+    return{
+        id: u.id,
+        firstName: u.first_name,
+        lastName: u.last_name,
+        middleName: u.middle_name ?? undefined,
+        bierthDate: u.birth_date.toISOString().slice(0,10),
+        email: u.email,
+        role: u.role.role_name,
+        status: u.status.status_name,
+    };
+}
